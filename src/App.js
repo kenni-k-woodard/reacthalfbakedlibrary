727b224ddcc2f960254ea8b71a2182979a701d2a
Kenni Woodard <kennikristine@gmail.com>
2023-01-23 16:37:18 -0800

    routes to books

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+import Main from './components/Main/Main.js';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/books" component={BookList} />
+      </Switch>
+    </div>
   );
 }
 
