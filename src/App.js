e95bb551a8113ab42d8ab2bbb0361e7b3d1d4ad8
Kenni Woodard <kennikristine@gmail.com>
2023-01-23 17:13:25 -0800

    books are clickable and route to appropriate detail page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import BookList from './components/book/BookList';
 import './App.css';
 import { Route, Switch, Link } from 'react-router-dom';
 import Main from './components/Main/Main.js';
+import BookDetail from './components/book/BookDetail.js';
 
 function App() {
   // TODO: Add routes to books & views
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/books" component={BookList} />
+        <Route exact path="/books/:id" component={BookDetail} />
       </Switch>
     </div>
   );
